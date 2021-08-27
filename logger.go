1bd912e2efb3968f701598b81ca58f440f14869d
barbarian <darkerbarbarian@gmail.com>
2021-08-27 20:03:31 +0300

    stoped to using log library

--- logger.go
+++ logger.go
@@ -3,7 +3,6 @@ package logger
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"runtime"
 	"strconv"
@@ -22,28 +21,36 @@ var (
 
 type Logger struct {
 	Mutex sync.Mutex
+	Directory string
+}
+
+func NewLogger (directory string) Logger{
+	return Logger{
+		Mutex: sync.Mutex{},
+		Directory: directory,
+	}
 }
 
 // InfoLog Logging function for Info messages. White message to console with Prefix INFO and Time
-func InfoLog(message string, data interface{}) {
+func (lo Logger)  InfoLog(message string, data interface{}) {
 	fmt.Println("INFO: ",time.Now().Format("15:04:05"), message, data)
 }
 // ErrorLog Logging function for Error messages. White message to console with Purple Prefix ERROR, Time and caller info
-func ErrorLog(message string, data interface{}) {
+func (lo Logger)  ErrorLog(message string, data interface{}) {
 	fmt.Println(colorPurple, "ERROR: ", colorReset,time.Now().Format("15:04:05"), getCaller(2), message, data)
 }
 
 // WarningLog Logging function for Warning messages. White message to console with Yellow Prefix Warning and Time
-func WarningLog(message string, data interface{}) {
-	fmt.Println(colorYellow, "WARNING: ", colorReset,time.Now().Format("15:04:05"), getCaller(2), message, data)
+func (lo Logger)  WarningLog(message string, data interface{}) {
+	fmt.Println(colorYellow, "WARNING: ", colorReset, time.Now().Format("15:04:05"), getCaller(2), message, data)
 }
 // FatalLog Logging function for Fatal messages. White message to console with Red Prefix FATAL, Time and caller info
-func FatalLog(message string, data interface{}) {
+func (lo Logger)  FatalLog(message string, data interface{}) {
 	fmt.Println(colorRed, "FATAL: ", colorReset,time.Now().Format("15:04:05"), getCaller(2), message, data)
 	os.Exit(1)
 }
 // FInfoLog Logging function for Info messages. White message to /logs/ directory with Prefix INFO and Time
-func(lo Logger)  FInfoLog(message string, data interface{}) {
+func (lo Logger)  FInfoLog(message string, data interface{}) {
 	lo.logFile(false, "INFO", message, data)
 }
 // FErrorLog Logging function for Error messages. White message to /logs/ directory with Prefix ERROR, Time and caller info
@@ -63,17 +70,20 @@ func (lo Logger) FFatalLog(message string, data interface{}) {
 func (lo Logger) logFile(needCaller bool, errLevel string, message string, data interface{}) error{
 	lo.Mutex.Lock()
 	defer lo.Mutex.Unlock()
-	path:=os.Getenv("PATH_TO_LOGS_FILES")
+	path:=lo.Directory
 	fileName := path+time.Now().Format("06 01 02")+".txt"
 	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0765)
 	defer file.Close()
 	if err != nil {
 		return err
 	}
-	newLog := log.New(file, errLevel+":\t", log.Ltime)
-	if needCaller {
-		newLog.Println(getCaller(3), message, data)
-	}else{newLog.Println(message, data)}
+		if needCaller {
+		line:=[]byte(errLevel+":\t"+time.Now().Format("15:04:05")+getCaller(3)+message+fmt.Sprint(data))
+		file.Write(line)
+	}else{
+		line:=[]byte(errLevel+":\t"+time.Now().Format("15:04:05")+message+fmt.Sprint(data))
+		file.Write(line)}
+
 
 	return nil
 }
