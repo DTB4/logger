7f580ac15c98f1dcd98bf07b6fb0a094346dbe8f
barbarian <darkerbarbarian@gmail.com>
2021-08-27 20:25:58 +0300

    too much doubledots in log fixed

--- logger.go
+++ logger.go
@@ -33,37 +33,37 @@ func NewLogger (directory string) Logger{
 
 // InfoLog Logging function for Info messages. White message to console with Prefix INFO and Time
 func (lo Logger)  InfoLog(message string, data interface{}) {
-	fmt.Println("INFO: ",time.Now().Format("15:04:05"), message, data)
+	fmt.Println("[INFO]: ",time.Now().Format("15:04:05"), message, data)
 }
 // ErrorLog Logging function for Error messages. White message to console with Purple Prefix ERROR, Time and caller info
 func (lo Logger)  ErrorLog(message string, data interface{}) {
-	fmt.Println(colorPurple, "ERROR: ", colorReset,time.Now().Format("15:04:05"), getCaller(2), message, data)
+	fmt.Println(colorPurple, "[ERROR]: ", colorReset,time.Now().Format("15:04:05"), getCaller(2), message, data)
 }
 
 // WarningLog Logging function for Warning messages. White message to console with Yellow Prefix Warning and Time
 func (lo Logger)  WarningLog(message string, data interface{}) {
-	fmt.Println(colorYellow, "WARNING: ", colorReset, time.Now().Format("15:04:05"), getCaller(2), message, data)
+	fmt.Println(colorYellow, "[WARNING]: ", colorReset, time.Now().Format("15:04:05"), getCaller(2), message, data)
 }
 // FatalLog Logging function for Fatal messages. White message to console with Red Prefix FATAL, Time and caller info
 func (lo Logger)  FatalLog(message string, data interface{}) {
-	fmt.Println(colorRed, "FATAL: ", colorReset,time.Now().Format("15:04:05"), getCaller(2), message, data)
+	fmt.Println(colorRed, "[FATAL]: ", colorReset,time.Now().Format("15:04:05"), getCaller(2), message, data)
 	os.Exit(1)
 }
 // FInfoLog Logging function for Info messages. White message to /logs/ directory with Prefix INFO and Time
 func (lo Logger)  FInfoLog(message string, data interface{}) {
-	lo.logFile(false, "INFO", message, data)
+	lo.logFile(false, "[INFO]", message, data)
 }
 // FErrorLog Logging function for Error messages. White message to /logs/ directory with Prefix ERROR, Time and caller info
 func(lo Logger)  FErrorLog(message string, data interface{}) {
-	lo.logFile(true,"ERROR", message, data)
+	lo.logFile(true,"[ERROR]", message, data)
 }
 // FWarningLog Logging function for Warning messages. White message to /logs/ directory with Prefix Warning and Time
 func(lo Logger)  FWarningLog(message string, data interface{}) {
-	lo.logFile(false,"WARNING", message, data)
+	lo.logFile(false,"[WARNING]", message, data)
 }
 // FFatalLog Logging function for Fatal messages. White message to /logs/ directory with Prefix FATAL, Time and caller info
 func (lo Logger) FFatalLog(message string, data interface{}) {
-	lo.logFile(true,"FATAL", message, data)
+	lo.logFile(true,"[FATAL]", message, data)
 	os.Exit(1)
 }
 
@@ -71,17 +71,17 @@ func (lo Logger) logFile(needCaller bool, errLevel string, message string, data 
 	lo.Mutex.Lock()
 	defer lo.Mutex.Unlock()
 	path:=lo.Directory
-	fileName := path+time.Now().Format("06 01 02")+".txt"
+	fileName := path+time.Now().Format("06_01_02")+".txt"
 	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0765)
 	defer file.Close()
 	if err != nil {
 		return err
 	}
 		if needCaller {
-		line:=[]byte(errLevel+":\t"+time.Now().Format("15:04:05")+":\t"+getCaller(3)+":\t"+message+":\t"+fmt.Sprint(data)+"\n")
+		line:=[]byte(errLevel+":\t"+time.Now().Format("15:04:05")+"\t"+getCaller(3)+"\t"+message+":\t"+fmt.Sprint(data)+"\n")
 		file.Write(line)
 	}else{
-		line:=[]byte(errLevel+":\t"+time.Now().Format("15:04:05")+":\t"+message+":\t"+fmt.Sprint(data)+"\n")
+		line:=[]byte(errLevel+":\t"+time.Now().Format("15:04:05")+"\t"+message+":\t"+fmt.Sprint(data)+"\n")
 		file.Write(line)}
 
 
